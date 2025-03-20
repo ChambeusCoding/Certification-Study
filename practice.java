9863333c49c44943a629ee47af7212e5d083f558
ChambeusCoding <180331000+ChambeusCoding@users.noreply.github.com>
2025-03-20 17:49:40 -0400

    gdfg

---
+++ practice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class practice {
+
+    public static void main(String[] args) {
+        int number = 5;
+        int number2 = 10;
+        int number3 = 15;
+
+        if(number<number2) {
+            System.out.println("Thats cool isnt it?");
+            System.out.println(number + number2);
+        } 
+        else {
+            System.out.println("Whatever");
+        }
+    // kd scanner = new Scanner();
+    Scanner kbd = new Scanner(System.in);
+    System.out.println("What dklsdfsdf; ");
+        
+
+    }
+
+  
+}
